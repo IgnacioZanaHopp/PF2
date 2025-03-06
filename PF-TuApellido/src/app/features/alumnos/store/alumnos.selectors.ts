590ef16ee4908becaeaf3391eeb245ebf76532ae
Ignacio Zañartu Hoppner <izanartuhoppner@icloud.com>
2025-03-05 21:47:06 -0300

    Primer commit

--- PF-TuApellido/src/app/features/alumnos/store/alumnos.selectors.ts
+++
@@ -1,9 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AlumnosState } from './alumnos.reducer';
-
-export const selectAlumnosState = createFeatureSelector<AlumnosState>('alumnos');
-
-export const selectAllAlumnos = createSelector(
-  selectAlumnosState,
-  state => state.alumnos
-);
